240a24952efd14ee2bc3ac65157c02a8f1ab620f
Ruby <rubiaalbuquerque@outlook.com>
2021-02-11 13:18:20 -0500

    Update code.py

--- code.py
+++ code.py
@@ -5,10 +5,10 @@ from datetime import date, timedelta
 #I'm using datetime so I can add the date to my file. It is usefull when you have to have to keep doing it every month 
 
 # building the date for the file's name
-first_day_of_this_month = date.today().replace(day=1)
-last_day_prev_month = first_day_of_this_month - timedelta(days=1)
-prev_month_name = last_day_prev_month.strftime('%B')
-# print(prev_month_name)
+first_day_current_month= date.today().replace(day=1)
+last_day_prev_month = first_day_current_month - timedelta(days=1)
+lastmonth_name = last_day_prev_month.strftime('%B')
+# print(lastmonth_name)
 
 now = date.today()
 last_month = now.month-1 if now.month > 1 else 12
@@ -36,5 +36,5 @@ split_column = data['column with the values to be used as a reference for splitt
 for value in split_column:
 
     df = data[data['Distributor'] == value]
-    output_file_name = "Name_your_file " + str(value) + "_" + str(prev_month_name) + "_" + str(currentyear) + ".csv"
+    output_file_name = "Name_your_file " + str(value) + "_" + str(lastmonth_name) + "_" + str(currentyear) + ".csv"
     df.to_csv(output_file_name)
